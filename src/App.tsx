fea768654c48efab9915eb10bb1b4728951cede0
rockcha <ojr5521@naver.com>
2025-11-05 09:26:25 +0900

    배포 준비

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-//import ButtonDemo from "./components/Demo/ButtonDemo";
-//import { SonnerDemo } from "./components/Demo/SonnerDemo";
-// import { CarouselDemo } from "./components/Demo/CarouselDemo";
-// import { TooltipDemo } from "./components/Demo/TooltipDemo";
-//import { AccordionDemo } from "./components/Demo/AccordionDemo";
-//import { Separator } from "@radix-ui/react-separator";
-
 import Header from "./components/layout/Header";
 
 function App() {
